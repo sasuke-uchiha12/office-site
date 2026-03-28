d596b9b89a50a0c16f6a88903cb5e826a81114da
Thulasi Vasan S <tryvasan02@gmail.com>
2026-03-28 18:02:54 +0000

    chore: enable vercel analytics

--- src/layout/AppShell.tsx
+++ src/layout/AppShell.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { useEffect, useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import { Footer } from "../components/Footer";
@@ -21,6 +22,7 @@ export function AppShell() {
         <Outlet />
       </main>
       <Footer />
+      <Analytics />
     </div>
   );
 }
